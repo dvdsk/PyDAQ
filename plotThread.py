ac945cdf4eb6eca57cc76bc2f46b67621f0ad56a
David Kleingeld <dskleingeld@gmail.com>
2017-08-14 12:57:55 +0200

    live plotting 10k points working for test data

--- plotThread.py
+++ plotThread.py
@@ -1,5 +1,5 @@
 import multiprocessing as mp
-
+import numpy as np
 import matplotlib
 #matplotlib.use('GTKAgg')
 from matplotlib import pyplot as plt
@@ -34,21 +34,48 @@ def testPipes(read_end, stop, outputShape):
 	canvas = ax.figure.canvas
 	while(not stop.is_set() and not read_end.poll(0.1)):
 		continue
-	data = read_end.recv()
-	line, = plt.plot(outputShape, data, animated=True)
-	background = canvas.copy_from_bbox(ax.bbox)
-	canvas.draw()
+		
+	##############INIT PLOT#####################
+	k=0.
+	x = np.linspace(0,50., num=10000)
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+
+	fig.canvas.draw()   # note that the first draw comes before setting data 
+
+	line, = ax.plot(x, lw=3)
+	ax.set_ylim([-1,1])
+
+
+	# cache the background
+	axbackground = fig.canvas.copy_from_bbox(ax.bbox)
+	##############DONE INIT PLOT#####################
 	
 	print("now in forever loopy loopy without break")
 	while(not stop.is_set()):
-		if(read_end.poll(0.1)):
+		if(read_end.poll()):
 			data = read_end.recv()
-			canvas.restore_region(background)
-			line.set_ydata(data)
-			ax.draw_artist(line)
-			canvas.blit(ax.bbox)
-			#plt.draw()
-			plt.pause(0.0001)
-			print("loop loop")
-	plt.show()
+			
+		line.set_ydata(np.sin(x/3.+k))
+		#print tx
+		k+=0.11
+
+		# restore background
+		fig.canvas.restore_region(axbackground)
+
+		# redraw just the points
+		ax.draw_artist(line)
+
+		# fill in the axes rectangle
+		fig.canvas.blit(ax.bbox)
+		# in this post http://bastibe.de/2013-05-30-speeding-up-matplotlib.html
+		# it is mentionned that blit causes strong memory leakage. 
+		# however, I did not observe that.
+
+		plt.pause(0.000000000001) 
+		#plt.pause calls canvas.draw(), as can be read here:
+		#http://bastibe.de/2013-05-30-speeding-up-matplotlib.html
+		#however with Qt4 (and TkAgg??) this is needed. It seems,using a different backend, 
+		#one can avoid plt.pause() and gain even more speed.
+		
 	print("testPipes rdy to join")
