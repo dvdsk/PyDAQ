3ada31b0af3ba463d66623380018d997fdc3eb8a
David Kleingeld <dskleingeld@gmail.com>
2017-08-15 16:11:21 +0200

    forgot files

---
+++ circBuff.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class circularNumpyBuffer:
+	"""A numpy implementation of a circular buffer, a circular buffer can append data forever
+	   but starts overwriting old data after its capacity is reached. It thus only remembers the
+	   last data. This simple implementation has no check on the appending data, make sure
+	   it is not larger then the capacity of this buffer. It is only capable of appending numpy arrays
+	   not other data.
+	"""
+	def __init__(self, capacity, dtype=np.float64):
+		#internal buffer is 10x larger to minimise the amount of move operations needed
+		self.arrLen = capacity*10
+		self.arr = np.empty(self.arrLen, dtype)
+
+		#indexes used to keep track of where the current data is
+		self.left_index = 0
+		self.right_index = 0
+		self.capacity = capacity
+	def append(self,sample):
+		#check if we have enough space left in the array
+		if(self.right_index+len(sample)<self.arrLen):
+			#copy the new data into the array
+			usedCapacity = self.right_index-self.left_index
+			self.arr[self.right_index : self.right_index+len(sample)] = sample
+			#check if we are at capacity and need to 'forget' some data by shifting the left index
+			if(usedCapacity + len(sample) > self.capacity):
+				self.left_index += len(sample)
+			self.right_index += len(sample)
+		else:
+			#start by forgetting the data we no longer need
+			newLeft = self.left_index+np.size(sample)
+			#copy the data from the end of the array to the start so we have enough space again
+			self.arr[0:(self.right_index - newLeft)] = self.arr[newLeft:self.right_index]
+			#update indexes reflecting we now start at the beginning of the array
+			self.right_index = self.right_index - newLeft
+			self.left_index = 0
+			#store sample and update right index 
+			#left index does not need updating as we started by updating left/forgetting some data
+			#and took this into account when moving data back to the the start of the array
+			self.arr[self.right_index : self.right_index+len(sample)] = sample
+			self.right_index += len(sample)
+	def acces(self):
+		#print(self.left_index)
+		#print(self.right_index)
+		return self.arr[self.left_index : self.right_index]
+	def __len__(self):
+		return self.right_index-self.left_index
---
+++ feedback.py
@@ -0,0 +1,117 @@
+import multiprocessing as mp
+
+from PyDAQmx import Task
+from PyDAQmx.DAQmxConstants import *
+from PyDAQmx.DAQmxTypes import *
+import numpy as np
+from circBuff import circularNumpyBuffer
+import time
+
+"""
+See documentation at: http://zone.ni.com/reference/en-XX/help/370471AA-01/
+
+-Constants are imported from PyDAQmx.DAQmxConstants
+
+-Variables that are not pointers can be used directly,
+as they will be automatically converted by ctypes
+
+-For pointers, first declare them and then use byref() 
+to pass by referenceNULL in C becomes None in Python
+"""
+class ReadTask(Task):
+	def __init__(self):
+		Task.__init__(self)
+		self.data = np.empty(200)
+		self.a = []
+		
+		#configurate input channel, where we read the ouput from the myDAQ
+		self.CreateAIVoltageChan(
+			"myDAQ1/ai0", #The name of the physical channel muDAQ1/aiN  (n= 0 or 1)
+			"", #name to assign to virt channel mapped to phys channel above
+			DAQmx_Val_Cfg_Default, #measurement technique used
+			-10.0, #min value expected to measure
+			10.0, #max value expected to measure
+			DAQmx_Val_Volts, #units for min val and max val
+			None) #name of custom scale if used
+
+		#configurate timing and sample rate for the samples
+		self.CfgSampClkTiming(
+			"", #source terimal of Sample clock ("" means onboard clock)
+			200.0, #sample rate (units: samples/second/channel)
+			DAQmx_Val_Rising, #aquire on rising edge of sample clock
+			DAQmx_Val_ContSamps, #aquire continues until task stopped
+			200) #numb to aquire if finitSamps/ bufferSize if contSamps (bufsize in this case)
+
+class WriteTask(Task):
+	def __init__(self):
+		Task.__init__(self)
+		self.sampswritten = int32()
+		self.a = []
+
+		#configurate output channel, this is the signal the myDAQ outputs
+		self.CreateAOVoltageChan(
+			"myDAQ1/ao0", #The name of the physical channel muDAQ1/aiN  (n= 0 or 1)
+			"", #name to assign to virt channel mapped to phys channel above
+			-10.0, #min value expected to output
+			10.0, #max value expected to output
+			DAQmx_Val_Volts, #units for min val and max val
+			None) #name of custom scale if used
+		#not configuring sample timing -> driver in on demand sample for anolog out
+
+
+def feedback(write_end, stop):
+	buffer = circularNumpyBuffer(10000, dtype=np.float64) #TODO expose to user (len)
+	data = np.empty(100, dtype=np.float64)
+	sampsWritten = int32()
+	sampsRead = int32()
+	
+	sendbuf = np.empty(2000)
+	start_idx = 0
+	
+	writeTask = WriteTask()
+	readTask = ReadTask()
+	t1 = t0 = 0
+	
+	feedbackSig = np.array([1], dtype=np.float64)#H(buffer.access)
+	n= 0
+	while(True):
+		t0 = t1
+		t1 = time.time()
+		print("latency:",t1-t0)
+		readTask.ReadAnalogF64(
+			DAQmx_Val_Auto, #read as many samples as there are in the buffer
+			0, #timeout in seconds
+			DAQmx_Val_GroupByChannel, #read entire channel in one go
+			data, #array where the samples should be put in
+			100, #number of samples
+			byref(sampsRead), #reference where to store: numb of samples read
+			None)
+		buffer.append(data)
+		if(n == 200):
+			write_end.send(sendbuf[0:start_idx])
+			start_idx=0
+			n=0
+		else:
+			sendbuf[start_idx:start_idx+sampsRead.value] = data[0:sampsRead.value]
+			start_idx+=sampsRead.value
+			n+=1
+		
+		# #een feedback funct
+		# def H(buffer):
+			# return 1*buffer.access[-1:-2]
+		
+		writeTask.WriteAnalogF64(
+			1, #number of samples to write
+			True, #start output automatically
+			1, #timeout to wait for funct to write samples 
+			DAQmx_Val_GroupByChannel, #read entire channel in one go
+			feedbackSig, #source array from which to write the data
+			byref(sampsWritten),  #variable to store the numb of written samps in
+			None)
+	
+	
+	
+
+
+if __name__ == "__main__":
+	Feedback()
