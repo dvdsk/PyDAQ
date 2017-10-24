128d566dca162e9240980ab6da55ed08f6faf606
David Kleingeld <dskleingeld@gmail.com>
2017-10-24 17:11:53 +0200

    now forgot to move aquireandgen example

--- Examples/aquireAndGen.py
+++ Examples/aquireAndGen.py
@@ -3,20 +3,15 @@ import numpy as np
 
 if __name__ == '__main__':
 
-	outputChannel1 = np.sin(np.linspace(0, 2*np.pi, num =1000,
-	                 endpoint=False, dtype=np.float64))
-	outputChannel2 = np.cos(np.linspace(0, 2*np.pi, num =1000, 
-	                 endpoint=False, dtype=np.float64))
-	outputPattern = outputChannel1 + outputChannel2
-
-
+	outputChannel1 = np.full(1000, 2, dtype=np.float64)
+	outputChannel2 = np.full(1000, 1, dtype=np.float64)
+	outputPattern = np.hstack((outputChannel1,outputChannel2))
+	
 	pd = pydaq.PyDAQ()
 
 	#configure
 	pd.setFileOptions(name="testData", format="csv")
-	pd.aquireAndGen(["myDAQ1/ai0","myDAQ1/ai1"],
-	                ["myDAQ1/ao0", "myDAQ1/ao1"],
-	                outputChannel1,samplerate=100)
+	pd.aquireAndGen(["myDAQ1/ai0","myDAQ1/ai1"],["myDAQ1/ao0","myDAQ1/ao1"],outputPattern,samplerate=100)
 
 	pd.begin()
 	pd.menu()
